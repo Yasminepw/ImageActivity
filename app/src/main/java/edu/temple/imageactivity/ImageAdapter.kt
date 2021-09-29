5062252ce612af366be108072ad2602cc417d51f
Yasminepw <yasminepwatkins@gmail.com>
2021-09-29 12:31:56 -0400

    Updated and finished ImageAdapter that inherits from RecyclerView.Adapter

--- app/src/main/java/edu/temple/imageactivity/ImageAdapter.kt
+++ app/src/main/java/edu/temple/imageactivity/ImageAdapter.kt
@@ -1,7 +1,36 @@
 package edu.temple.imageactivity
 
+import android.app.Activity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 
-class ImageAdapter() {
+class ImageAdapter (private val dataSet: List<Images>, activity: Activity) : RecyclerView.Adapter<ImageAdapter.Viewer>() {
+    var imageView: ImageView = activity.findViewById(R.id.imageView)
+    var textView: TextView = activity.findViewById(R.id.titleTextView)
 
+    class Viewer(_itemView: View) : RecyclerView.ViewHolder(_itemView) {
+        val imageView : ImageView = _itemView.findViewById(R.id.imageViewLayout)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Viewer {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.`image_main`, parent, false)
+        return Viewer(view)
+    }
+
+
+    override fun onBindViewHolder(holder: Viewer, position: Int) {
+        holder.imageView.setImageResource(dataSet[position].resourceId)
+
+        holder.imageView.setOnClickListener{
+            imageView.setImageResource(dataSet[position].resourceId)
+            textView.text = dataSet[position].title
+        }
+    }
+
+    override fun getItemCount() = 12
 
 }
