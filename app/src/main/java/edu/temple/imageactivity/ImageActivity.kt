ba44309e4f92ae1d88e3aa521614ec75c8b5a383
Yasminepw <yasminepwatkins@gmail.com>
2021-09-29 13:17:57 -0400

    Updated ImageActivity

--- app/src/main/java/edu/temple/imageactivity/ImageActivity.kt
+++ app/src/main/java/edu/temple/imageactivity/ImageActivity.kt
@@ -2,10 +2,19 @@ package edu.temple.imageactivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
 
 class ImageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val adapter = ImageAdapter(photoList(), this)
+        val mRecyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        mRecyclerView.layoutManager = GridLayoutManager(this, 2)
+        mRecyclerView.adapter = adapter
+
+
     }
 }
